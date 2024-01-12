ce2bee9403170931723a78e4e1011fc330db7c13
tangpoo <lhinav@naver.com>
2024-01-12 14:42:27 +0900

    student_class

---
+++ src/main/java/model/subject/Subject.java
@@ -0,0 +1,27 @@
+package subject;
+
+import model.subject.SubjectType;
+
+public class Subject {
+    private final Long id;
+    private final String name;
+    private final SubjectType type;
+
+    public Subject(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = SubjectType.parse(type);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SubjectType getType() {
+        return type;
+    }
+}
