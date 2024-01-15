175477d981d421fa4619b43e0046d8a3106f21b5
tangpoo <lhinav@naver.com>
2024-01-15 11:30:58 +0900

    manager update

--- src/main/java/manager/StudentManager.java
+++ src/main/java/manager/StudentManager.java
@@ -3,6 +3,7 @@ package manager;
 import db.dao.DAO;
 import db.dto.StudentDTO;
 import model.student.Student;
+import model.subject.Subject;
 
 import java.util.List;
 
@@ -13,8 +14,8 @@ public class StudentManager {
         this.dao = dao;
     }
 
-    public void addUser(String name){
-        dao.save(new StudentDTO(name));
+    public void addUser(String name, List<Subject> subjectList){
+        dao.save(new StudentDTO(name, subjectList));
     }
 
     public List<Student> getAllUser(){
